7ee91a68988484e8f837c880f6bb65a483f312b3
Nick Pizzo <nick.pizzo@gmail.com>
2016-05-27 00:20:41 -0700

    initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,36 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = () => {
+  return (
+    <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">
+              React Timer App
+            </li>
+            <li>
+              <IndexLink to="/" activeClassName="active-link">
+                Timer
+              </IndexLink>
+            </li>
+            <li>
+              <Link to="/" activeClassName="active-link">
+                Countdown
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="http://nickpizzo.me" target="_blank">Nick Pizzo</a>
+            </li>
+          </ul>
+        </div>
+
+    </div>
+  )
+};
+
+module.exports = Navigation;
