9d9a68f7fd33ae6f780051ddd8822d18a6d8444d
Nick Pizzo <nick.pizzo@gmail.com>
2016-05-30 16:55:25 -0700

    basic routing

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 //^^used ES6 destructuring syntax to pull off four variables from react router
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
@@ -14,6 +16,9 @@ require('style!css!sass!applicationStyles')
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
+      <IndexRoute component={Timer}/>
+      <Route path='timer' component={Timer}/>
+      <Route path='countdown' component={Countdown}/>
 
     </Route>
   </Router>,
